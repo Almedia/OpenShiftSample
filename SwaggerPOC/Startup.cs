0763673c536d61db89a9f79b07c01f135262ceaa
Bahadır Özdemir <bahadirozdemir@Bahadrs-MacBook-Pro.local>
2019-02-17 11:40:16 +0300

    first commit

---
+++ SwaggerPOC/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SwaggerPOC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerGen(c=>{
+                c.SwaggerDoc("v1", new Info { Title = "JsonEndPoint", Version = "1.0"});
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), "swagger")),
+                RequestPath = "/swagger"
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/JsonEndPoint.json", "Banka Json Servisleri V1");
+                    c.SwaggerEndpoint("/swagger/XmlEndPoint.json","Banka Xml Servisleri V1");
+                    c.RoutePrefix = "";
+                });
+
+            app.UseMvc();
+        }
+    }
+}
